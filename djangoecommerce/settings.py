2eff4fee4101a5d0d7847bf4926495b6d0abe669
Phat <lvp3795@gmail.com>
2023-01-07 00:45:33 +0700

    4

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ["static"]
+STATIC_ROOT = 'static/staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
